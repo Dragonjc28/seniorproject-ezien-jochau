bf5fd75d2b357c7444142caba69f389964765dc7
Jolan <chaujolan@gmail.com>
2020-05-19 03:08:19 -0700

    implemented size increasing feature
    added partial point system but no ui yet,
     - so far all objects are worth the same amount (1 point) and scaling works as every 5 points doubles size
    -added potential map objects with adjusted box colliders to work with sinkhole

---
+++ Assets/Scripts/Conditions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditions : MonoBehaviour
+{
+    public int Points = 0;
+    public OnChangePos SinkScript;    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.collider.gameObject);
+        UpdatePoints();
+    }
+
+    private void UpdatePoints()
+    {
+        Points++ ;
+
+        if(Points % 5 == 0)
+        {
+            StartCoroutine(SinkScript.IncreaseSize());
+        }
+    }
+}
---
+++ Assets/Scripts/OnChangePos.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnChangePos : MonoBehaviour
+{
+    public PolygonCollider2D hole2dCollider;
+    public PolygonCollider2D ground2dCollider;
+    public MeshCollider GenMeshCollider;
+    public Collider GroundCollider;
+    public float scale = 0.5f;
+    Mesh GenMesh;
+
+    public IEnumerator IncreaseSize()
+    {
+        Vector3 InitScale = transform.localScale;
+        Vector3 FinScale = InitScale * 2;
+
+        float t = 0;
+        while (t <= 0.4f)
+        {
+            t += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(InitScale, FinScale, t);
+            yield return null;
+        }
+    }
+
+    private void Start()
+    {
+        GameObject[] objects = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        foreach (var obj in objects)
+        {
+            if (obj.layer == LayerMask.NameToLayer("Obstacles"))
+            {
+                Physics.IgnoreCollision(obj.GetComponent<Collider>(), GenMeshCollider, true);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(transform.hasChanged == true)
+        {
+            transform.hasChanged = false;
+            hole2dCollider.transform.position = new Vector2(transform.position.x, transform.position.z);
+            hole2dCollider.transform.localScale = transform.localScale * scale;
+            MakeHole();
+            Make3dMeshCollider();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Physics.IgnoreCollision(other, GroundCollider, true);
+        Physics.IgnoreCollision(other, GenMeshCollider, false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Physics.IgnoreCollision(other, GroundCollider, false);
+        Physics.IgnoreCollision(other, GenMeshCollider, true);
+    }
+
+    private void MakeHole()
+    {
+        Vector2[] positions = hole2dCollider.GetPath(0);
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = hole2dCollider.transform.TransformPoint(positions[i]);
+        }
+
+        ground2dCollider.pathCount = 2;
+        ground2dCollider.SetPath(1, positions);
+    }
+
+    private void Make3dMeshCollider()
+    {
+        if (GenMesh!=null) Destroy(GenMesh);
+        GenMesh = ground2dCollider.CreateMesh(true, true);
+        GenMeshCollider.sharedMesh = GenMesh;
+    }
+}
